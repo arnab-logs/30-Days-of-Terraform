ecfbb6925f6508dc3dc61798fbf04de0a29354d7
Arnab Nandi <aninquisitivemindatwork@gmail.com>
2025-11-27 00:06:38 +0530

    Completed Day 3 task

--- day-03/main.tf
+++ day-03/main.tf
@@ -11,12 +11,22 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-#Create an S3 bucket
+# Create a VPC
+resource "aws_vpc" "main_vpc" {
+  cidr_block           = "10.0.0.0/16"
+
+  tags = {
+    Name = "day3-vpc"
+  }
+}
+
+# Create an S3 bucket with dependency
 resource "aws_s3_bucket" "day3_bucket" {
   bucket = "arnab-day3-terraform"
 
   tags = {
     Name        = "arnab day3 bucket"
     Environment = "Dev"
+    VPC         = aws_vpc.main_vpc.id
   }
 }
